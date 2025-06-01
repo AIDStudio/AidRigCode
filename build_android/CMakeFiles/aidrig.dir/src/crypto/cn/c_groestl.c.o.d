9ab0679824d8069a59dc5d6a21bd11f8b574b7dc
AIDStudio <aidstudio21@gmail.com>
2025-06-01 22:15:50 +0200

    Build fájlok feltöltése

---
+++ build_android/CMakeFiles/aidrig.dir/src/crypto/cn/c_groestl.c.o.d
@@ -0,0 +1,20 @@
+CMakeFiles/aidrig.dir/src/crypto/cn/c_groestl.c.o: \
+  /storage/emulated/0/aidrig/project/project/aidrigs/src/crypto/cn/c_groestl.c \
+  /storage/emulated/0/aidrig/project/project/aidrigs/src/crypto/cn/c_groestl.h \
+  /data/data/com.termux/files/usr/lib/clang/20/include/stdint.h \
+  /data/data/com.termux/files/usr/include/stdint.h \
+  /data/data/com.termux/files/usr/include/bits/wchar_limits.h \
+  /data/data/com.termux/files/usr/include/sys/cdefs.h \
+  /data/data/com.termux/files/usr/include/android/versioning.h \
+  /data/data/com.termux/files/usr/include/android/api-level.h \
+  /data/data/com.termux/files/usr/include/bits/get_device_api_level_inlines.h \
+  /data/data/com.termux/files/usr/include/android/ndk-version.h \
+  /data/data/com.termux/files/usr/lib/clang/20/include/stddef.h \
+  /data/data/com.termux/files/usr/lib/clang/20/include/__stddef_header_macro.h \
+  /data/data/com.termux/files/usr/lib/clang/20/include/__stddef_ptrdiff_t.h \
+  /data/data/com.termux/files/usr/lib/clang/20/include/__stddef_size_t.h \
+  /data/data/com.termux/files/usr/lib/clang/20/include/__stddef_wchar_t.h \
+  /data/data/com.termux/files/usr/lib/clang/20/include/__stddef_null.h \
+  /data/data/com.termux/files/usr/lib/clang/20/include/__stddef_offsetof.h \
+  /storage/emulated/0/aidrig/project/project/aidrigs/src/crypto/cn/hash.h \
+  /storage/emulated/0/aidrig/project/project/aidrigs/src/crypto/cn/groestl_tables.h
